3e86348e02be9fcc78741f57268b64ad12ce750c
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-11-07 10:18:12 +0000

    setup routers and tailwindcss

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from './views/App'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
