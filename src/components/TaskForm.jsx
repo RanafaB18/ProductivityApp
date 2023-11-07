707a03a302eace92bdfbefdfb04d9b00a1dd6744
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-11-07 16:51:42 +0000

    Added tooltips

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,6 +2,7 @@ import CancelIcon from "./CancelIcon";
 import PropTypes from "prop-types"
 import SendIcon from "./SendIcon";
 import PriorityList from "./PriorityList";
+import { Tooltip } from "react-tooltip";
 const TaskForm = ({onCloseForm}) => {
   return (
     <div className="absolute top-1 bg-white shadow-lg border p-2 px-3 w-full rounded-lg">
@@ -15,12 +16,18 @@ const TaskForm = ({onCloseForm}) => {
           className="outline-none placeholder:text-xs"
           placeholder="Description"
         />
+        <Tooltip noArrow id="priorities"  className="!p-1 !px-2"/>
+        <div data-tooltip-offset={2} data-tooltip-content={"Set priority p1, p2, p3, p4"} data-tooltip-id="priorities" className="w-fit">
+
         <PriorityList />
+        </div>
         <div className="flex justify-end gap-2">
-          <button type="button" onClick={onCloseForm} className="bg-[#f5f5f5] hover:bg-gray-300  rounded-md p-1">
+            <Tooltip noArrow id="cancel" className="!p-1 !px-2"/>
+            <Tooltip noArrow id="send"  className="!p-1 !px-2"/>
+          <button type="button" data-tooltip-offset={2} data-tooltip-id="cancel" data-tooltip-content={"Cancel"} onClick={onCloseForm} className="bg-[#f5f5f5] hover:bg-gray-300  rounded-md p-1">
             <CancelIcon />
           </button>
-          <button type="submit" className="bg-[#dc4c3e] hover:bg-red-600 rounded-md p-1">
+          <button data-tooltip-offset={2} data-tooltip-content={"Send"} data-tooltip-id="send" type="submit" className="bg-[#dc4c3e] hover:bg-red-600 rounded-md p-1">
             <SendIcon />
           </button>
         </div>
