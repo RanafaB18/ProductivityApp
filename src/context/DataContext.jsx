7f40140e3a5596ede38758be0836d64aa827a02d
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-11-07 19:32:02 +0000

    Dawn of the Context

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const DataContext = createContext()
+
+// eslint-disable-next-line react/prop-types
+export const DataContextProvider = ({ children }) => {
+    const [tasks, setTasks] = useState([])
+    return (
+        <DataContext.Provider value={{
+            tasks,
+            setTasks
+        }}>
+            { children }
+        </DataContext.Provider>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './views/App'
-
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./views/App";
+import { DataContextProvider } from "./context/DataContext";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />
-  }
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+    path: "/",
+    element: <App />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <DataContextProvider>
+    <RouterProvider router={router} />
+  </DataContextProvider>
+);
