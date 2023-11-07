9f8c9418d593f634cd6d70fe75486022b8150a38
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-11-07 22:45:49 +0000

    List the tasks and color codes priorities

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types"
+
+
+const TaskItem = ({ task }) => {
+    const priorityToColorMapping = {
+        'P1': 'border-red-600 bg-red-500 hover:bg-red-200 text-red-600',
+        'P2': 'border-orange-600 bg-orange-500 hover:bg-orange-200 text-orange-500',
+        'P3': 'border-blue-600 bg-blue-500 hover:bg-blue-200 text-blue-500',
+        'P4': 'hover:bg-gray-200'
+    }
+    const { taskName, description, priority } = task
+    console.log("priority", priority);
+  return (
+    <article className="flex items-start gap-3 py-3 min-h-[80px]">
+      <div className={`border bg-white ${priorityToColorMapping[priority] } bg-opacity-25 group transition duration-500 cursor-pointer rounded-full h-fit p-1 mt-0.5`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={`w-3 h-3 rotate-3 opacity-0 group-hover:opacity-100 transition duration-500`}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 12.75l6 6 9-13.5"
+          />
+        </svg>
+      </div>
+      <div className="flex flex-col gap-1 max-w-sm">
+        <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">{ taskName }</p>
+        <p className="text-xs opacity-70 overflow-hidden whitespace-nowrap text-ellipsis">{ description }</p>
+      </div>
+    </article>
+  );
+};
+
+TaskItem.propTypes = {
+    task: PropTypes.object
+}
+export default TaskItem;
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -2,20 +2,42 @@ import Header from "../components/Header";
 import Menu from "../components/Menu";
 import beginnerImage from "../assets/beginner-image.jpg";
 import AddTask from "../components/AddTask";
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext";
+import TaskItem from "../components/TaskItem";
 const App = () => {
+  const { tasks } = useContext(DataContext);
   return (
     <main className="p-5">
       <Menu />
       <section className="py-3 px-8">
         <section className="flex flex-col divide-y">
           <Header />
+          {
+            tasks.length !== 0 && (
+              tasks.map((task) => {
+                return <TaskItem key={task.id} task={task} />;
+              })
+            )
+          }
           <AddTask />
         </section>
-        <section aria-label="todos" className="flex flex-col items-center max-w-xs mx-auto">
-          <img src={beginnerImage} alt="" className="w-fit"/>
-          <p className="font-semibold text-center">What do you need to get done today?</p>
-          <p className="text-center text-sm opacity-70">By default, tasks added here will be due today. Click + to add a task</p>
-        </section>
+        {tasks.length === 0 && (
+          <section
+            aria-label="todos"
+            className="flex flex-col items-center max-w-xs mx-auto"
+          >
+            <img src={beginnerImage} alt="" className="w-fit" />
+            <p className="font-semibold text-center">
+              What do you need to get done today?
+            </p>
+            <p className="text-center text-sm opacity-70">
+              By default, tasks added here will be due today. Click + to add a
+              task
+            </p>
+          </section>
+        )
+        }
       </section>
     </main>
   );
