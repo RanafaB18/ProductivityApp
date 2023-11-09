d7ab2d9ef111f8148e37f979665e215fe78b25f4
j-Abban <justice.abban@amalitech.org>
2023-11-09 08:16:05 +0000

    Finished login

--- src/views/ResetPassword.jsx
+++ src/views/ResetPassword.jsx
@@ -1,27 +1,26 @@
+import { useState } from "react"
+
+useState
 const Resetpassword = () => {
-  return (
+return (
     <>
-    <div className="signup-container">
+    <div className="resetPassword-container">
     <h2>Reset password</h2>
-     <form >
-     <div className="form-group">
+    <form >
+    <div className="password-form">
             <label htmlFor="name">New Password:</label>
             <input type="text" id="name" name="name" placeholder="Enter Your New Password" required />
         </div>
-        <div className="form-group">
+        <div className="password-form">
             <label htmlFor="confirmPassword">Confirm New Password:</label>
-            <input type="confirmPassword" id="comfirmPassword" name="confirmPassword" placeholder="Confirm Password" required />
+            <input type="text" id="comfirmPassword" name="confirmPassword" placeholder="Confirm Password" required />
         </div>
-        <div className="form-group">
-        <input type="submit" value="reset password" />
+        <div className="password-form">
+            <button className="btn" type="submit">reset password</button>
     </div>
-     </form>
-    
-
+    </form>
     </div>
-
     </>
-  )
+)
 }
-
 export default Resetpassword
--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -4,30 +4,43 @@ import { Link } from 'react-router-dom'
 export const SignUp = () => {
 return (
     <>
-
 <div className="signup-container">
     <h2>Sign Up</h2>
-    
     <form  className="signup-form">
         <div className="form-group">
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" placeholder='Justice Abban'required />
+            <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder='Justice Abban'required />
         </div>
         <div className="form-group">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="justiceabban002@gmail.com" required />
+            <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="justiceabban002@gmail.com" required />
         </div>
     <div className="form-group">
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" placeholder="8+ characters" name="password" required/>
+        <input
+        type="password"
+        id="password"
+        placeholder="8+ characters" name="password" required/>
     </div>
     <div className="form-group">
         <label htmlFor="password">Confirm Password:</label>
-        <input type="password" id="ConfirmPassword" placeholder="ConfirmPassword" name="ConfirmPassword" required/>
+        <input
+        type="password"
+        id="ConfirmPassword"
+        placeholder="ConfirmPassword"
+        name="ConfirmPassword" required/>
     </div>
     <div className="form-group">
     <p className="form-group">Already have an account? <Link to={'/login'}>LogIn</Link></p>
-        <input type="submit" value="sign Up" />
+        <button className='btn' type="submit"> sign Up</button>
     </div>
     </form>
 </div>
