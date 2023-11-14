edae4c7dd68f267e61739173683e51994ecd9242
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-11-14 00:36:57 +0000

    Added 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,45 @@
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './views/App'
-import Login from './views/Login'
-import { SignUp } from './views/SignUp'
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./views/App";
+import Login from "./views/Login";
+import { SignUp } from "./views/SignUp";
 import { DataContextProvider } from "./context/DataContext";
-import Resetpassword from './views/ResetPassword'
-import Report from './views/Report'
-
+import Resetpassword from "./views/ResetPassword";
+import Report from "./views/Report";
+import NotFound from "./views/NotFound";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <SignUp />
+    path: "/",
+    element: <SignUp />,
+    errorElement: <NotFound />,
   },
   {
-    path: '/today',
-    element: <DataContextProvider><App /></DataContextProvider>,
+    path: "/today",
+    element: (
+      <DataContextProvider>
+        <App />
+      </DataContextProvider>
+    ),
+    errorElement: <NotFound />,
   },
   {
-    path: '/login',
-    element: <Login />
+    path: "/login",
+    element: <Login />,
+    errorElement: <NotFound />,
   },
   {
-    path: '/reset-password',
-    element: <Resetpassword />
+    path: "/reset-password",
+    element: <Resetpassword />,
+    errorElement: <NotFound />,
   },
   {
-    path: '/performance',
-    element: <Report />
-  }
-
-])
+    path: "/performance",
+    element: <Report />,
+    errorElement: <NotFound />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
+  <RouterProvider router={router} />
 );
---
+++ src/views/NotFound.jsx
@@ -0,0 +1,10 @@
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p className="text-4xl font-bold">404, Page Not Found</p>
+    </div>
+  )
+}
+
+export default NotFound
